94d23e4c1eca453332db307a8636fddfb9e5684d
fahmi <fahmibambe@gmail.com>
2025-03-25 13:53:14 +0700

    access-control rev

--- src/features/access-controls/components/data-table-toolbar.tsx
+++ src/features/access-controls/components/data-table-toolbar.tsx
@@ -16,10 +16,12 @@ export function DataTableToolbar<TData>({
     <div className='flex items-center justify-between'>
       <div className='flex flex-1 flex-col-reverse items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2'>
         <Input
-          placeholder='Search department...'
-          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          placeholder='Search room name...'
+          value={
+            (table.getColumn('roomName')?.getFilterValue() as string) ?? ''
+          }
           onChange={(event) =>
-            table.getColumn('name')?.setFilterValue(event.target.value)
+            table.getColumn('roomName')?.setFilterValue(event.target.value)
           }
           className='h-8 w-[150px] lg:w-[250px]'
         />
--- src/features/access-controls/index.tsx
+++ src/features/access-controls/index.tsx
@@ -25,9 +25,11 @@ export default function AccessControls() {
         <div className='mb-2 flex items-center justify-between space-y-2 flex-wrap gap-x-4'>
           <div>
             <h2 className='text-2xl font-bold tracking-tight'>
-              AccessControls
+              Access Controls
             </h2>
-            <p className='text-muted-foreground'>Here a List Department!</p>
+            <p className='text-muted-foreground'>
+              Manage your access control device here
+            </p>
           </div>
           <AccessControlsPrimaryButtons />
         </div>
